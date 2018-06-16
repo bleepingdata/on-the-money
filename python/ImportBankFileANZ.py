a052889b35d2103512f4cc1aebac5c0715903c1a
bleepingdata <stephenhillranger@outlook.com>
2018-06-16 16:54:13 +1200

    Added parameters to our import script

--- python/ImportBankFileANZ.py
+++ python/ImportBankFileANZ.py
@@ -1,15 +1,53 @@
 import pandas as pd
+import pyodbc
 import sqlalchemy as sa
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey
 
+import argparse
+from argparse import ArgumentParser
+import os.path
+
+parser = ArgumentParser()
+bankaccountargs = parser.add_mutually_exclusive_group()
+parser.add_argument("-f", "--file", dest="bankexcelfile", required=True,
+                    help="the file name of the ANZ excel file to import", metavar="FILE")
+bankaccountargs.add_argument("-ban", "--bankaccountnumber", dest="bankaccountnumber", required=False,
+                    help="load transactions into specified bank account")
+bankaccountargs.add_argument("-bad", "--bankaccountdescription", dest="bankaccountdescription", required=False,
+                    help="load transactions into specified bank account")
+args = parser.parse_args()
+
+try:
+    with open(args.bankexcelfile) as file:
+        pass
+except IOError as e:
+    print("Unable to open file") #Does not exist OR no read permissions
+    raise
+
+# Set parameter-related variables
+bankexcelfile = args.bankexcelfile
+bankaccountnumber = args.bankaccountnumber
+bankaccountdescription = args.bankaccountdescription
+
+# Create the SQL connection object
 engine = create_engine('mssql://LOCALHOST\\SQLEXPRESS/OnTheMoney?trusted_connection=yes;driver=SQL+Server+Native+Client+10.0') 
 
+# Grab a pyodbc cursor to use for calling stored procs
+cursor = engine.raw_connection().cursor()
 
-file = 'C:\\Stephen\\Test\\on-the-money\\ANZTestFileExport.xlsx'
+# Prepare for the import (this truncates a table and checks if accounts exists)
+cursor.execute("BOOKS.PrepareForImportFile ?, ?", [bankaccountnumber, bankaccountdescription])
+cursor.commit()
+
 # Load spreadsheet
-xl = pd.ExcelFile(file)
+xl = pd.ExcelFile(bankexcelfile)
 
-# Load a sheet into a DataFrame by name: df1
-df1 = xl.parse('Transactions',converters={'Amount':str,'Balance':str})
-df1.to_sql(name='LoadImportFile', if_exists='append',con=engine, schema='BOOKS', index=False, chunksize=1)
+# Load a sheet from the spreadsheet into a DataFrame. For ANZ, the sheet we need is named "Transactions"
+dfTransactions = xl.parse('Transactions',converters={'Amount':str,'Balance':str})
+dfTransactions.to_sql(name='LoadImportFile', if_exists='append',con=engine, schema='BOOKS', index=False, chunksize=1)
+
+# Process the file
+cursor.execute("BOOKS.ProcessImportFile ?, ?", [bankaccountnumber, bankaccountdescription])
+cursor.commit()
+
 
--- sql/stored-procs/BOOKS.ProcessImportFile.sql
+++ sql/stored-procs/BOOKS.ProcessImportFile.sql
@@ -2,52 +2,72 @@ USE OnTheMoney
 GO
 DROP PROC BOOKS.ProcessImportFile
 GO
-CREATE PROC BOOKS.ProcessImportFile @BankAccountId INT
+CREATE PROC BOOKS.ProcessImportFile @BankAccountNumber NVARCHAR(56) = NULL, @BankAccountDescription NVARCHAR(50) = NULL
 AS
 BEGIN
+-- Process a bank file from ANZ. The file contents must already exist in BOOKS.LoadImportFile table. Parameters determine which account the
+-- transactions will be recorded against
 
-DECLARE @ImportUniqueIdentifier UNIQUEIDENTIFIER = NEWID();
+	SET NOCOUNT ON;
 
-INSERT BOOKS.[Transaction] (BankTransactionDate, BankProcessedDate, TransactionXML, Amount, ImportUniqueIdentifier)
-SELECT 
-	TRY_CONVERT(DATE, [Transaction Date]) AS [BankTransactionDate], TRY_CONVERT(DATE, [Processed Date]) AS [BankProcessedDate],
-		(SELECT [LoadImportFileId]
-				  ,[Transaction Date] AS [TransactionDate]
-				  ,[Processed Date] AS [ProcessedDate]
-				  ,[Type]
-				  ,[Details]
-				  ,[Particulars]
-				  ,[Code]
-				  ,[Reference]
-				  ,[Amount]
-				  ,[Balance]
-				  ,[To/From Account Number] AS [ToFromAccountNumber]
-				  ,[Conversion Charge] AS [ConversionCharge]
-				  ,[Foreign Currency Amount] AS [ForeignCurrencyAmount]
-			  FROM [BOOKS].[LoadImportFile] B
-			  WHERE B.LoadImportFileId = A.LoadImportFileId
-			  FOR XML PATH ('Row'), Type
-			  ) 
-			AS [TransactionXML],
-		BOOKS.CleanStringMoney([Amount]) AS [Amount],
-		@ImportUniqueIdentifier
-	FROM [BOOKS].[LoadImportFile] A
+	IF @BankAccountNumber IS NULL AND @BankAccountDescription IS NULL
+	BEGIN
+		RAISERROR('All parameters for proc are null or missing', 15,1);
+	END
 
-	INSERT [BOOKS].TransactionLine (TransactionId, AccountId, DepositAmount, WithdrawalAmount)
-	SELECT TransactionId, 
-		CASE WHEN Amount >= 0 THEN @BankAccountId ELSE 0 END AS [AccountId],
-		ABS(Amount) AS [DepositAmount],
-		0 AS WithdrawalAmount
-		FROM BOOKS.[Transaction] 
-		WHERE ImportUniqueIdentifier = @ImportUniqueIdentifier
-	UNION ALL
-		 SELECT TransactionId, 
-		CASE WHEN Amount < 0 THEN @BankAccountId ELSE 0 END AS [AccountId],
-		0 AS DepositAmount,
-		ABS(Amount) AS [WithdrawalAmount]
-		FROM BOOKS.[Transaction] 
-		WHERE ImportUniqueIdentifier = @ImportUniqueIdentifier
-		ORDER BY TransactionId ASC;
+	-- get the bank account id
+	DECLARE @BankAccountId INT = (SELECT AccountId FROM BOOKS.Account 
+									WHERE 
+										BankAccountNumber = ISNULL(@BankAccountNumber, BankAccountNumber) 
+										AND Description = ISNULL(@BankAccountDescription, Description)
+										AND (@BankAccountNumber IS NOT NULL OR @BankAccountDescription IS NOT NULL));
+	IF @BankAccountId IS NULL
+	BEGIN
+		RAISERROR('Unable to find BOOKS.Account entry for bank account %s, description %s', 15,1, @BankAccountNumber, @BankAccountDescription);
+	END
+
+	DECLARE @ImportUniqueIdentifier UNIQUEIDENTIFIER = NEWID();
+
+	INSERT BOOKS.[Transaction] (BankTransactionDate, BankProcessedDate, TransactionXML, Amount, ImportUniqueIdentifier)
+	SELECT 
+		TRY_CONVERT(DATE, [Transaction Date]) AS [BankTransactionDate], TRY_CONVERT(DATE, [Processed Date]) AS [BankProcessedDate],
+			(SELECT [LoadImportFileId]
+					  ,[Transaction Date] AS [TransactionDate]
+					  ,[Processed Date] AS [ProcessedDate]
+					  ,[Type]
+					  ,[Details]
+					  ,[Particulars]
+					  ,[Code]
+					  ,[Reference]
+					  ,[Amount]
+					  ,[Balance]
+					  ,[To/From Account Number] AS [ToFromAccountNumber]
+					  ,[Conversion Charge] AS [ConversionCharge]
+					  ,[Foreign Currency Amount] AS [ForeignCurrencyAmount]
+				  FROM [BOOKS].[LoadImportFile] B
+				  WHERE B.LoadImportFileId = A.LoadImportFileId
+				  FOR XML PATH ('Row'), Type
+				  ) 
+				AS [TransactionXML],
+			BOOKS.CleanStringMoney([Amount]) AS [Amount],
+			@ImportUniqueIdentifier
+		FROM [BOOKS].[LoadImportFile] A
+
+		INSERT [BOOKS].TransactionLine (TransactionId, AccountId, DepositAmount, WithdrawalAmount)
+		SELECT TransactionId, 
+			CASE WHEN Amount >= 0 THEN @BankAccountId ELSE 0 END AS [AccountId],
+			ABS(Amount) AS [DepositAmount],
+			0 AS WithdrawalAmount
+			FROM BOOKS.[Transaction] 
+			WHERE ImportUniqueIdentifier = @ImportUniqueIdentifier
+		UNION ALL
+			 SELECT TransactionId, 
+			CASE WHEN Amount < 0 THEN @BankAccountId ELSE 0 END AS [AccountId],
+			0 AS DepositAmount,
+			ABS(Amount) AS [WithdrawalAmount]
+			FROM BOOKS.[Transaction] 
+			WHERE ImportUniqueIdentifier = @ImportUniqueIdentifier
+			ORDER BY TransactionId ASC;
 
 
 
