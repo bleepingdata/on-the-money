812414d47aed075e435a7c02b5f6e816d45e0718
bleepingdata <stephenhillranger@outlook.com>
2018-06-25 21:03:08 +1200

    Added proc to calculate monthly account balances

---
+++ sql/stored-procs/BOOKS.GetAccountBalanceMonthly.sql
@@ -0,0 +1,36 @@
+USE OnTheMoney
+GO
+ALTER PROC BOOKS.GetAccountBalanceMonthly
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+	DECLARE @CurrentAccountId INT = (SELECT MIN(AccountId) FROM BOOKS.Account);
+	DECLARE @MonthlyAccountBalance TABLE (AccountId INT, BalanceDate DATE, Balance MONEY);
+
+	WHILE EXISTS (SELECT * FROM BOOKS.Account WHERE AccountId = @CurrentAccountId)
+	BEGIN
+
+		DECLARE @CurrentMonth DATE = '2017-01-01';
+		DECLARE @EOCurrentMonth DATE;
+
+		WHILE @CurrentMonth < GETDATE()
+		BEGIN
+	
+			SET @EOCurrentMonth = EOMONTH(@CurrentMonth);
+
+			INSERT @MonthlyAccountBalance EXEC BOOKS.GetAccountBalanceAtDate @AccountId=@CurrentAccountId, @Date=@EOCurrentMonth
+
+			SET @CurrentMonth = DATEADD(MONTH, 1, @CurrentMonth);
+		END
+
+		SET @CurrentAccountId = (SELECT MIN(AccountId) FROM BOOKS.Account WHERE AccountId > @CurrentAccountId);
+
+	END
+
+	SELECT a.Description, a.AccountCode, FORMAT(mab.BalanceDate, 'MMM yyyy') AS [BalanceDateDescription], mab.BalanceDate, mab.Balance FROM @MonthlyAccountBalance mab
+		INNER JOIN BOOKS.Account a ON mab.AccountId = a.AccountId
+		ORDER BY a.AccountCode ASC, mab.BalanceDate ASC;
+
+END
+GO
