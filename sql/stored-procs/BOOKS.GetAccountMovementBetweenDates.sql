a9d93ecb8ccbee6222218dbe8802bd3367731eea
bleepingdata <stephenhillranger@outlook.com>
2018-06-26 22:15:45 +1200

    added procs to calculate account movements

---
+++ sql/stored-procs/BOOKS.GetAccountMovementBetweenDates.sql
@@ -0,0 +1,40 @@
+USE OnTheMoney
+GO
+DROP PROC BOOKS.GetAccountMovementBetweenDates
+GO
+CREATE PROC BOOKS.GetAccountMovementBetweenDates @AccountId INT, @StartDate DATE, @EndDate DATE
+AS
+BEGIN
+/* 
+ Get the balance for an account at a date, taking the opening balance into account
+ */
+	SET NOCOUNT ON;
+
+		DECLARE @DepositAmountTotal MONEY 
+			= ISNULL(
+						(SELECT SUM(DepositAmount) 
+							FROM BOOKS.[Transaction] t
+								INNER JOIN BOOKS.TransactionLine tl ON t.TransactionId = tl.TransactionId
+                                INNER JOIN BOOKS.Account a  ON tl.AccountId = a.AccountId
+								WHERE ISNULL(t.BankProcessedDate, '2100-01-01') >= a.OpeningBalanceDate
+                                    AND a.AccountId = @AccountId
+									AND t.BankProcessedDate >= @StartDate
+                                    AND t.BankProcessedDate <= @EndDate)
+							,0);
+		DECLARE @WithdrawalAmountTotal MONEY 
+			= ISNULL(
+						(SELECT SUM(WithdrawalAmount) 
+							FROM BOOKS.[Transaction] t
+								INNER JOIN BOOKS.TransactionLine tl ON t.TransactionId = tl.TransactionId
+								INNER JOIN BOOKS.Account a  ON tl.AccountId = a.AccountId
+								WHERE ISNULL(t.BankProcessedDate, '2100-01-01') >= a.OpeningBalanceDate
+                                    AND a.AccountId = @AccountId
+                                    AND t.BankProcessedDate >= @StartDate
+                                    AND t.BankProcessedDate <= @EndDate)
+							,0);
+
+
+SELECT @AccountId AS [AccountId], @StartDate as [StartDate], @EndDate as [EndDate],  (@DepositAmountTotal - @WithdrawalAmountTotal) AS [Movement]
+
+END
+GO
