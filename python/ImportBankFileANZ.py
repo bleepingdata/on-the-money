72e4c621245167efa5e42feef94f25cffb7fbcb6
Stephen Hill-Ranger <stephen@Stephens-iMac.local>
2018-08-08 11:17:47 +1200

    Can no import from Python to Postgres.  Now need to work on processing.

--- python/ImportBankFileANZ.py
+++ python/ImportBankFileANZ.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import pyodbc
+#import pyodbc
+import psycopg2
 import sqlalchemy as sa
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey
 
@@ -10,7 +11,7 @@ import os.path
 parser = ArgumentParser()
 parser.add_argument("-f", "--file", dest="bankexcelfile", required=True,
                     help="the file name of the ANZ excel file to import", metavar="FILE")
-parser.add_argument("-dsn", "--datasourcename", dest="datasourcename", required=True,
+parser.add_argument("-db", "--databasename", dest="databasename", required=True,
                     help="data source name of the database server")
 parser.add_argument("-u", "--username", dest="username", required=True,
                     help="username for the database server connection")
@@ -18,10 +19,30 @@ parser.add_argument("-p", "--password", dest="password", required=True,
                     help="password for the database server connection")
 bankaccountargs = parser.add_mutually_exclusive_group()
 bankaccountargs.add_argument("-ban", "--bankaccountnumber", dest="bankaccountnumber", required=False,
-                    help="load transactions into specified bank account")
+                     help="load transactions into specified bank account")
 bankaccountargs.add_argument("-bad", "--bankaccountdescription", dest="bankaccountdescription", required=False,
                     help="load transactions into specified bank account")
 args = parser.parse_args()
+
+
+# Set parameter-related variables
+s_bankexcelfile = args.bankexcelfile
+s_databasename = args.databasename
+s_username = args.username
+s_password = args.password
+s_bankaccountnumber = args.bankaccountnumber
+s_bankaccountdescription = args.bankaccountdescription
+s_removeoverlappingtransactions = True
+ 
+conn = psycopg2.connect(database = s_databasename, user = s_username, password = s_password, host = "localhost", port = "5432")
+print ("Opened database successfully")
+
+cur = conn.cursor()
+
+cur.execute("select BOOKS.PrepareForImportFile (NULL,'Current Account');")
+conn.commit
+
+conn.close()
 
 try:
     with open(args.bankexcelfile) as file:
@@ -30,40 +51,31 @@ except IOError as e:
     print("Unable to open file") #Does not exist OR no read permissions
     raise
 
-# Set parameter-related variables
-bankexcelfile = args.bankexcelfile
-datasourcename = args.datasourcename
-username = args.username
-password = args.password
-bankaccountnumber = args.bankaccountnumber
-bankaccountdescription = args.bankaccountdescription
-removeoverlappingtransactions = True; 
 
-# Create the SQL connection object
+# # Create the SQLconnection object
 
-# Connection From Windows
+# # Connection From Windows
+
+engine = create_engine('postgresql://postgres:lilian99@localhost:5432/onthemoney')
 # engine = create_engine('mssql://LOCALHOST\\SQLEXPRESS/OnTheMoney?trusted_connection=yes;driver=SQL+Server+Native+Client+10.0') 
 
 #Connection From MacOS using FreeTDS and unixODBC https://github.com/mkleehammer/pyodbc/wiki/Connecting-to-SQL-Server-from-Mac-OSX
-pyodbc_connection='mssql+pyodbc://{}:{}@{}'.format(username, password, datasourcename)
-engine = create_engine(pyodbc_connection) 
+# pyodbc_connection='mssql+pyodbc://{}:{}@{}'.format(username, password, datasourcename)
+# engine = create_engine(pyodbc_connection) 
 
-# Grab a pyodbc cursor to use for calling stored procs
-cursor = engine.raw_connection().cursor()
+# # Grab a pyodbc cursor to use for calling stored procs
+# cursor = engine.raw_connection().cursor()
 
-# Prepare for the import (this truncates a table and checks if accounts exists)
-cursor.execute("BOOKS.PrepareForImportFile ?, ?", [bankaccountnumber, bankaccountdescription])
-cursor.commit()
+# # Prepare for the import (this truncates a table and checks if accounts exists)
+# cursor.execute("BOOKS.PrepareForImportFile ?, ?", [bankaccountnumber, bankaccountdescription])
+# cursor.commit()
 
-# Load spreadsheet
-xl = pd.ExcelFile(bankexcelfile)
+# # Load spreadsheet
+xl = pd.ExcelFile(s_bankexcelfile)
 
-# Load a sheet from the spreadsheet into a DataFrame. For ANZ, the sheet we need is named "Transactions"
+# # Load a sheet from the spreadsheet into a DataFrame. For ANZ, the sheet we need is named "Transactions"
 dfTransactions = xl.parse('Transactions',converters={'Amount':str,'Balance':str})
-dfTransactions.to_sql(name='LoadImportFile', if_exists='append',con=engine, schema='BOOKS', index=False, chunksize=1)
+dfTransactions.to_sql(name='loadimportfile', if_exists='append',con=engine, schema='books', index=False, chunksize=1)
 
-# Process the file
-cursor.execute("BOOKS.ProcessImportFile ?, ?, ?", [bankaccountnumber, bankaccountdescription, removeoverlappingtransactions])
-cursor.commit()
-
-
+# # Process the file
+# cursor.execute("BOOKS.ProcessImportFile ?, ?, ?", [bankaccountnumber, bankaccountdescription, removeoverlappingtransactions])
---
+++ sql/drop-scripts/drop-tables-and-schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE BOOKS.TransactionLine;
+DROP TABLE BOOKS.Transaction;
+drop table BOOKS.transactionlinestaging;
+DROP TABLE BOOKS.transactionstaging;
+DROP TABLE BOOKS.LoadImportFile;
+DROP TABLE BOOKS.loadimportfile_excel_anzmortgage;
+drop table BOOKS.TransactionImportRules;
+DROP TABLE BOOKS.Account;
+DROP TABLE BOOKS.AccountType;
+DROP SCHEMA BOOKS;
