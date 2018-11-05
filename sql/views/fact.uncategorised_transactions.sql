670cf68c0651fdb414707e986bcd46b2f154385e
Stephen Hill-Ranger <stephenhillranger@outlook.com>
2018-11-05 22:14:42 +1300

    added movement fact

--- sql/views/fact.uncategorised_transactions.sql
+++ sql/views/fact.uncategorised_transactions.sql
@@ -1,6 +1,7 @@
-create or replace view fact.account_balance
+create or replace view fact.uncategorised_transactions
 AS
-select asm.accountid, a.description, asm.year, asm.month, asm.month_as_date, asm.balance
+select ?
 from fact.account_summary_by_month asm
 	inner join books.account a on asm.accountid = a.accountid
+	inner join books.accounttype a_t on a.accounttypeid = a_t.accounttypeid
 order by asm.accountid, asm.year, asm.month;
