ee74842aaaec08acc807b19ac962a047cb37597f
stephenhillranger@outlook.com <thisismyhangout@gmail.com>
2019-04-20 11:17:32 +1200

    Fixed deletion scripts

---
+++ sql/drop-scripts/drop-functions.sql
@@ -0,0 +1,9 @@
+drop function books.process_transaction_import_rules;
+drop function books.prepare_import;
+drop function books.last_day;
+drop function books.insert_transaction_import_rule;
+drop function books.get_account_balance_at_date;
+drop function books.process_file_anz_excel;
+drop function books.calculate_balance_all;
+drop function books.calculate_balance;
+drop function fact.populate_account_summary_by_month;
