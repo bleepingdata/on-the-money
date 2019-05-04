3c2cc5b5322fe01c9b1888f8885520f22dbf681a
stephenhillranger@outlook.com <thisismyhangout@gmail.com>
2019-05-05 08:02:12 +1200

    added script to drop views and fixed all missing or invalid other objects

--- sql/drop-scripts/drop-functions.sql
+++ sql/drop-scripts/drop-functions.sql
@@ -1,9 +1,19 @@
-drop function books.process_transaction_import_rules;
-drop function books.prepare_import;
-drop function books.last_day;
-drop function books.insert_transaction_import_rule;
+drop function bank.delete_bank_transaction_entries_for_account;
+drop function bank.insert_bank_transaction_from_anz_excel;
+drop function bank.insert_bank_transaction_from_anz_mortgage_excel;
+drop function bank.insert_import_rule;
+drop function bank.process_import_rules;
+
+drop function books.delete_gl_entries_for_account;
 drop function books.get_account_balance_at_date;
-drop function books.process_file_anz_excel;
-drop function books.calculate_balance_all;
-drop function books.calculate_balance;
+drop function books.insert_gl_entry_basic;
+drop function books.insert_gl_entry_from_bank_transaction;
+drop function books.insert_gl_from_bank_import;
+drop function books.insert_journal_entry_basic;
+drop function books.last_day;
+
 drop function fact.populate_account_summary_by_month;
+
+drop function load.prepare_anz_excel;
+drop function load.prepare_anz_mortgage_excel;
+
--- sql/drop-scripts/drop-tables-and-schema.sql
+++ sql/drop-scripts/drop-tables-and-schema.sql
@@ -1,24 +1,22 @@
-drop view fact.account;
-drop view fact.account_balance;
-drop view fact.account_movement;
-drop view fact.transactions;
-drop view fact.uncategorised_transactions;
-
-drop table books.general_ledger;
-DROP TABLE BOOKS.TransactionLine;
-DROP TABLE BOOKS.Transaction;
-drop table BOOKS.transactionlinestaging;
-DROP TABLE BOOKS.transactionstaging;
-DROP TABLE BOOKS.LoadImportFile;
-DROP TABLE BOOKS.loadimportfile_excel_anzmortgage;
-drop table BOOKS.TransactionImportRules;
-DROP TABLE BOOKS.Account;
-DROP TABLE BOOKS.Account_Type;
 
 drop table fact.account_summary_by_month;
 
+drop table books.general_ledger;
 
-DROP SCHEMA BOOKS;
-drop schema fact;
-drop schema stage;
+drop table bank.transaction;
+drop table bank.import_rule;
+
+drop table dimension.dates;
+
+drop table load.anz_excel;
+drop table load.anz_mortgage_excel;
+
+drop table books.account;
+drop table books.account_type;
+
+
+drop schema bank;
+drop schema books;
 drop schema dimension;
+drop schema fact;
+drop schema load;
