ece129f7e2ee705409d80c074abca51b141d8c4a
stephenhillranger@outlook.com <thisismyhangout@gmail.com>
2019-05-05 21:40:32 +1200

    removing zero balance accounts from balance fact

--- sql/views/fact.account_balance.sql
+++ sql/views/fact.account_balance.sql
@@ -4,4 +4,5 @@ create or replace view fact.account_balance
 AS
 select asm.account_id, year, asm.month_number, asm.month_end_date, asm.balance
 from fact.account_summary_by_month asm
+where asm.balance <> 0
 order by asm.account_id, asm.year, asm.month_number;
