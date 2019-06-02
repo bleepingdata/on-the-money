4507c62b8f2b168fbc630aab9951d20632578857
stephenhillranger@outlook.com <thisismyhangout@gmail.com>
2019-06-02 12:44:40 +1200

    fixed up bank mortgage balances

--- sql/functions/bank.process_import_rules.sql
+++ sql/functions/bank.process_import_rules.sql
@@ -23,6 +23,19 @@ begin
 		) matches
 		WHERE t_to_update.transaction_id = matches.transaction_id
 			and t_to_update.bank_account_id <> matches.other_party_account_id;
+	
+	-- GL transactions where the account id does not match the import rules (usually because new rules have been added)
+	update books.general_ledger as gl
+		set account_id = matches.other_party_account_id
+		from 
+		(
+		select gl.gl_id, gl.account_id, t.other_party_account_id 
+		from books.general_ledger gl 
+		inner join bank."transaction" t on gl.source_identifier = t.transaction_id
+	where gl.account_id <> t.bank_account_id
+		and gl.account_id <> t.other_party_account_id
+	) matches
+	where gl.gl_id = matches.gl_id;
 
 END;
 $$ language plpgsql;
---
+++ sql/views/fact.bank_transfers.sql
@@ -0,0 +1,26 @@
+drop view if exists fact.bank_transfers;
+
+create or replace view fact.bank_transfers
+AS
+select gl.gl_id, 
+	gl.gl_type_id, 
+	gl.gl_date, 
+	gl.gl_grouping_id, 
+	gl.row_creation_date, 
+	gl.account_id, 
+	gl_other_party.account_id as other_party_account_id,
+	a_other_party.description as other_party_description,
+	gl.debit_amount, 
+	gl.credit_amount, 
+	gl.memo, 
+	gl.source_identifier
+from books.general_ledger gl
+inner join books.account a_bank_transfers 
+	on gl.account_id = a_bank_transfers.account_id
+inner join books.general_ledger gl_other_party 
+	on gl.gl_grouping_id = gl_other_party.gl_grouping_id 
+		and gl_other_party.account_id <> gl.account_id
+inner join books.account a_other_party 
+	on gl_other_party.account_id = a_other_party.account_id
+where a_bank_transfers.description='Bank Transfers'
+;
