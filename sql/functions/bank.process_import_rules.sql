d721230fdf0d8271bc6baaed280e07935bcea660
stephenhillranger@outlook.com <thisismyhangout@gmail.com>
2019-06-05 21:37:48 +1200

    fixed bug in wildcard handling where wildcard entries screwed everything else up

--- sql/functions/bank.process_import_rules.sql
+++ sql/functions/bank.process_import_rules.sql
@@ -4,6 +4,9 @@ create or replace function bank.process_import_rules ()
 RETURNS void AS $$
 begin
 	
+	
+	-- First sweep.  Do wildcard rules.
+
 	update bank."transaction" as t_to_update
 	set other_party_account_id = matches.other_party_account_id -- select *
 	from 
@@ -14,29 +17,42 @@ begin
 					and ir.start_date <= t.processed_date 
 					and ir.end_date >= t.processed_date
 		where 
-			(t.type = ir.type or ir.type is null)
+			ir.wildcard_field is not null 
+			and 
+				(
+				t.details LIKE ir.wildcard_field
+				or t.particulars LIKE ir.wildcard_field
+				or t.code LIKE ir.wildcard_field
+				or t.reference LIKE ir.wildcard_field
+				)
+
+		) matches
+		WHERE t_to_update.transaction_id = matches.transaction_id
+			and t_to_update.bank_account_id <> matches.other_party_account_id;
+		
+	
+	-- Second sweep.  Do all other rules. This sweep can overwrite wildcard rules.
+	
+	update bank."transaction" as t_to_update
+	set other_party_account_id = matches.other_party_account_id -- select *
+	from 
+	(
+		select t.bank_account_id, t.transaction_id, ir.other_party_account_id
+		from bank."transaction" t
+			inner join bank.import_rule ir on t.bank_account_id = ir.bank_account_id 
+					and ir.start_date <= t.processed_date 
+					and ir.end_date >= t.processed_date
+		where 
+			ir.wildcard_field is null
+			and
+			((t.type = ir.type or ir.type is null)
 			and (t.other_party_bank_account_number = ir.other_party_bank_account_number or ir.other_party_bank_account_number is null)
 			and (
-			
-					(
-						(t.details is not null or t.particulars is not null or t.code is not null or t.reference is not null)
-						and (t.details LIKE ir.details or ir.details is null)
-						and (t.particulars LIKE ir.particulars or ir.particulars is null)
-						and (t.code LIKE ir.code or ir.code is null)
-						and (t.reference LIKE ir.reference or ir.reference is null)
-					)
---				or 
---					(
---						ir.wildcard_field is not null 
---						and 
---							(
---							t.details LIKE ir.wildcard_field
---							or t.particulars LIKE ir.wildcard_field
---							or t.code LIKE ir.wildcard_field
---							or t.reference LIKE ir.wildcard_field
---							)
---					)
-			)
+					(t.details LIKE ir.details or ir.details is null)
+					and (t.particulars LIKE ir.particulars or ir.particulars is null)
+					and (t.code LIKE ir.code or ir.code is null)
+					and (t.reference LIKE ir.reference or ir.reference is null)
+			))
 		) matches
 		WHERE t_to_update.transaction_id = matches.transaction_id
 			and t_to_update.bank_account_id <> matches.other_party_account_id;
@@ -49,7 +65,7 @@ begin
 		select gl.gl_id, gl.account_id, t.other_party_account_id 
 		from books.general_ledger gl 
 		inner join bank."transaction" t on gl.source_identifier = t.transaction_id
-		where (gl.account_id <> t.bank_account_id
+		where (gl.account_id <> t.bank_account_id  -- get the other side of the transaction
 			and gl.account_id <> t.other_party_account_id)
 			or (t.other_party_account_id is not null and gl.account_id in (select account_id from books.account where description in ('uncategorised expense', 'uncategorised income')))
 	) matches
