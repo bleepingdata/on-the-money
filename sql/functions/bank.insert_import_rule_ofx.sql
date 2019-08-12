71a91a1c82cd97c7bb28680e022b30d909b6b2ea
Stephen Hill <stephenhillranger@outlook.com>
2019-08-12 17:36:15 +1200

    added import rule wrapper

--- sql/functions/bank.insert_import_rule_ofx.sql
+++ sql/functions/bank.insert_import_rule_ofx.sql
@@ -8,32 +8,28 @@ create or replace function bank.insert_import_rule_ofx
 	s_ofx_name varchar(50),
 	s_ofx_memo varchar(255),
 	n_priority int2)
-returns int as $$
+returns void as $$
 declare n_bank_account_id int;
 n_account_id int;
 n_other_party_account_id int;
 n_import_rule_id int;
 begin
 
+
 	if n_priority is null
     then
     	n_priority=32767;  -- by default, ofx  rules should have a high priority
     end if;
-    
-    select bank_account_id into n_bank_account_id from bank.account where description = s_bank_account;
-    select account_id into n_account_id from books.account where description = s_account;
-	select account_id into n_other_party_account_id from books.account where description = s_other_party_account;
 
-	if n_other_party_account_id is null
-	then 
-		raise exception 'unable to insert transaction import rule because s_other_party_account %s cannot be found', s_other_party_account;
-		return 0;
-	end if;
+	perform bank.insert_import_rule(s_import_rule_type:='Standard',
+	s_bank_account:=s_bank_account,
+	s_account:=s_account,
+	s_other_party_account:=s_other_party_account,
+    s_type:=s_type,
+    s_ofx_name:=s_ofx_name,
+    s_ofx_memo:=s_ofx_memo,
+    n_priority:=n_priority);
 
-	insert into bank.import_rule (bank_account_id, account_id, other_party_account_id, type, ofx_name, ofx_memo)
-		values (n_bank_account_id, n_account_id, n_other_party_account_id, s_type, s_ofx_name, s_ofx_memo)
-	 RETURNING import_rule_id into n_import_rule_id;
-	
-	return n_import_rule_id;
+	return;
 end;
 $$ language plpgsql;
