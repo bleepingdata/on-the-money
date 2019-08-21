0c56a385b335080bfc2190dbe4a589119bbd64c9
Stephen Hill <stephenhillranger@outlook.com>
2019-08-22 11:10:27 +1200

    fixed bug with dimension views and account type

--- sql/views/dimension.account_type.sql
+++ sql/views/dimension.account_type.sql
@@ -2,5 +2,5 @@ drop view if exists dimension.account_type;
 
 create or replace view dimension.account_type
 AS
-select at.account_type_id, at.description as "account_type"
+select at.account_type_id, at.account_type as "account_type"
 from books.account_type at;
