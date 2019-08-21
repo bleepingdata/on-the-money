85652793d11c25e77b41a756da8c3b56c575c16a
Stephen Hill <stephenhillranger@outlook.com>
2019-08-22 11:10:27 +1200

    fixed bug with dimension views and account type

--- sql/views/dimension.account.sql
+++ sql/views/dimension.account.sql
@@ -2,6 +2,6 @@ drop view if exists dimension.account;
 
 create or replace view dimension.account
 AS
-select a.account_id, a.description, a_t.description as account_type, a.open_date, a.close_date
+select a.account_id, a.description, a_t.account_type as account_type, a.open_date, a.close_date
 from books.account a
 	inner join books.account_type a_t on a.account_type_id = a_t.account_type_id;
