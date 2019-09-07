4b1c179a182298d0b86f9d2107505a9d0c49758a
Stephen Hill <stephenhillranger@outlook.com>
2019-09-07 12:33:18 +1200

    some change or other

--- sql/views/fact.account_movement.sql
+++ sql/views/fact.account_movement.sql
@@ -3,6 +3,8 @@ drop view if exists fact.account_movement;
 create or replace view fact.account_movement
 AS
 select asm.account_id, asm.year, asm.month_number, asm.month_end_date, asm.debit_amount, asm.credit_amount, 
-	asm.debit_amount-asm.credit_amount as movement, (asm.debit_amount-asm.credit_amount - asm.debit_amount_last_month - asm.credit_amount_last_month) AS difference_from_last_month
+	asm.debit_amount-asm.credit_amount as movement, 
+	(asm.debit_amount-asm.credit_amount - asm.debit_amount_last_month - asm.credit_amount_last_month) AS difference_from_last_month,
+	lag(asm.debit_amount-asm.credit_amount, 1) over (order by account_id, year, month_number) as movement_last_month 
 from fact.account_summary_by_month asm
 order by asm.account_id, asm.year, asm.month_number;
