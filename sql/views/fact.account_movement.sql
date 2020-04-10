929323fc9ed2ed1d9df5d39814a12e039e3d1b86
stephenhillranger@outlook.com <thisismyhangout@gmail.com>
2020-04-10 13:38:55 +1200

    Added movement_last_year field for trend lines

--- sql/views/fact.account_movement.sql
+++ sql/views/fact.account_movement.sql
@@ -5,6 +5,7 @@ AS
 select asm.account_id, asm.year, asm.month_number, asm.month_end_date, asm.debit_amount, asm.credit_amount, 
 	asm.debit_amount-asm.credit_amount as movement, 
 	(asm.debit_amount-asm.credit_amount - asm.debit_amount_last_month - asm.credit_amount_last_month) AS difference_from_last_month,
-	lag(asm.debit_amount-asm.credit_amount, 1) over (order by account_id, year, month_number) as movement_last_month 
+	lag(asm.debit_amount-asm.credit_amount, 1) over (order by account_id, year, month_number) as movement_last_month,
+	lag(asm.debit_amount-asm.credit_amount,12) over (order by account_id, year, month_number) as movement_last_year
 from fact.account_summary_by_month asm
 order by asm.account_id, asm.year, asm.month_number;
